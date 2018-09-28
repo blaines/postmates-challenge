0c86d10a3f8fe81824882acfd47f5a8ce617fe4c
Blaine <blaine@Blaines-MacBook-Air.local>
2018-09-27 22:54:38 -0700

    Google apparently charges for geocoding, using a free service

---
+++ src/app/geocoders/openstreet.py
@@ -0,0 +1,9 @@
+import json
+import urllib
+import logging
+import app.config
+
+class HereGeocoder:
+    def __init__(self):
+        self.service_url = f""
+    def geocode(self, address = "default"):
